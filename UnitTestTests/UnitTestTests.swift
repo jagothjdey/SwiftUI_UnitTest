5bdfe5b34c70d12b32c9dd8a7ae23c823eacc2d7
dey <jagothjyotidey91@gmail.com>
2020-05-06 18:08:36 +0200

    First Commit

--- UnitTestTests/UnitTestTests.swift
+++ UnitTestTests/UnitTestTests.swift
@@ -1,34 +1,14 @@
-//
-//  UnitTestTests.swift
-//  UnitTestTests
-//
-//  Created by Jagoth Jyoti Dey on 06.05.20.
-//  Copyright © 2020 Jagoth. All rights reserved.
-//
-
 import XCTest
+import ViewInspector // 1.
 @testable import UnitTest
 
-class UnitTestTests: XCTestCase {
+extension ContentView: Inspectable { } // 2.
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+final class ContentViewTests: XCTestCase {
+
+    func testStringValue() throws { // 3.
+        let sut = ContentView()
+        let value = try sut.inspect().vStack().hStack(1).text(0).string()
+        XCTAssertEqual(value, "World")
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
